2e1f2416e801a64598a91fbb76ae2801575ca037
JaredSchwartz <75581425+JaredSchwartz@users.noreply.github.com>
2024-06-12 21:57:56 -0600

    added tests for loader and apriori

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using RuleMiner
 using Test
 
-@testset "RuleMiner.jl" begin
-    # Write your tests here.
+@testset "loader.jl" begin
+    data = load_transactions(joinpath(@__DIR__,"files/testdata.txt"),:wide; sep=',')
+    @test size(data.matrix) == (9,16)
+    @test sum(data2.matrix) == 36
+    @test data2.colkeys[10] == "hamburger"
+    @test data2.linekeys[7] == "7"
+end
+
+@testset "apriori.jl" begin
+    data = load_transactions(joinpath(@__DIR__,"files/testdata.txt"),:wide; sep=',')
+    rules = apriori(data,0.3,2)
+    @test rules.RHS == ["beer","bread","cheese","eggs","ham","milk","milk","eggs"]
 end
