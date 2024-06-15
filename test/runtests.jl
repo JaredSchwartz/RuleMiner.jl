bcca05900a5cb144fbeb84a8551e5fcbd29a1584
JaredSchwartz <75581425+JaredSchwartz@users.noreply.github.com>
2024-06-15 11:52:19 -0600

    updated tests to test both absolute and relative supports

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,21 +11,48 @@ end
 
 @testset "apriori.jl" begin
     data = load_transactions(joinpath(@__DIR__,"files/testdata.txt"),:wide)
-    rules = apriori(data,0.3,5)
-    sorted = sort(rules,[:Support,:RHS])
-    @test sorted.LHS == [String[], String[], String[], String[], ["milk"], ["eggs"], String[], String[]]
-    @test sorted.RHS == ["beer", "bread", "cheese", "ham", "eggs", "milk", "eggs", "milk"]
-    @test sorted.Support == [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.4444444444444444, 0.4444444444444444, 0.5555555555555556, 0.5555555555555556]
-    @test sorted.Confidence ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.7999999999999999, 0.7999999999999999, 0.5555555555555556, 0.5555555555555556]
-    @test sorted.Coverage ≈ [1.0, 1.0, 1.0, 1.0, 0.5555555555555556, 0.5555555555555556, 1.0, 1.0]
-    @test sorted.Lift ≈ [1.0, 1.0, 1.0, 1.0, 1.4399999999999997, 1.4399999999999997, 1.0, 1.0]
-    @test sorted.N == [3, 3, 3, 3, 4, 4, 5, 5]
-    @test sorted.Length == [1, 1, 1, 1, 2, 2, 1, 1]
+
+    @testset "percentage support" begin
+        rules = apriori(data,0.3,5)
+        sorted = sort(rules,[:Support,:RHS])
+        @test sorted.LHS == [String[], String[], String[], String[], ["milk"], ["eggs"], String[], String[]]
+        @test sorted.RHS == ["beer", "bread", "cheese", "ham", "eggs", "milk", "eggs", "milk"]
+        @test sorted.Support == [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.4444444444444444, 0.4444444444444444, 0.5555555555555556, 0.5555555555555556]
+        @test sorted.Confidence ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.7999999999999999, 0.7999999999999999, 0.5555555555555556, 0.5555555555555556]
+        @test sorted.Coverage ≈ [1.0, 1.0, 1.0, 1.0, 0.5555555555555556, 0.5555555555555556, 1.0, 1.0]
+        @test sorted.Lift ≈ [1.0, 1.0, 1.0, 1.0, 1.4399999999999997, 1.4399999999999997, 1.0, 1.0]
+        @test sorted.N == [3, 3, 3, 3, 4, 4, 5, 5]
+        @test sorted.Length == [1, 1, 1, 1, 2, 2, 1, 1]
+    end
+
+    @testset "absolute support" begin
+        rules = apriori(data,2,5)
+        sorted = sort(rules,[:Support,:RHS])
+        @test sorted.LHS == [String[], String[], String[], String[], ["milk"], ["eggs"], String[], String[]]
+        @test sorted.RHS == ["beer", "bread", "cheese", "ham", "eggs", "milk", "eggs", "milk"]
+        @test sorted.Support == [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.4444444444444444, 0.4444444444444444, 0.5555555555555556, 0.5555555555555556]
+        @test sorted.Confidence ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.7999999999999999, 0.7999999999999999, 0.5555555555555556, 0.5555555555555556]
+        @test sorted.Coverage ≈ [1.0, 1.0, 1.0, 1.0, 0.5555555555555556, 0.5555555555555556, 1.0, 1.0]
+        @test sorted.Lift ≈ [1.0, 1.0, 1.0, 1.0, 1.4399999999999997, 1.4399999999999997, 1.0, 1.0]
+        @test sorted.N == [3, 3, 3, 3, 4, 4, 5, 5]
+        @test sorted.Length == [1, 1, 1, 1, 2, 2, 1, 1]
+    end
 end
 
 @testset "eclat.jl" begin
     data = load_transactions(joinpath(@__DIR__,"files/testdata.txt"),:wide)
-    sets = eclat(data,3)
-    @test sets.Itemset == [["beer"], ["bread"], ["cheese"], ["ham"], ["eggs"], ["eggs", "milk"], ["milk"]]
-    @test sets.Support == [3, 3, 3, 3, 5, 4, 5]
+    
+    @testset "percentage support" begin
+        sets = eclat(data,0.3)
+        @test sets.Itemset == [["beer"], ["bread"], ["cheese"], ["ham"], ["eggs"], ["eggs", "milk"], ["milk"]]
+        @test sets.Support ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.5555555555555556, 0.4444444444444444, 0.5555555555555556]
+        @test sets.N == [3, 3, 3, 3, 5, 4, 5]
+    end
+    
+    @testset "asbolute support" begin
+        sets = eclat(data,3)
+        @test sets.Itemset == [["beer"], ["bread"], ["cheese"], ["ham"], ["eggs"], ["eggs", "milk"], ["milk"]]
+        @test sets.Support ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.5555555555555556, 0.4444444444444444, 0.5555555555555556]
+        @test sets.N == [3, 3, 3, 3, 5, 4, 5]
+    end
 end
