7681e7bc467104dfbf6445830713839a70567e35
JaredSchwartz <75581425+JaredSchwartz@users.noreply.github.com>
2024-06-29 10:29:54 -0600

    reverted testing against hashes as julia 1.6 doesn't use the same hashing algorithm as 1.10+

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,24 +8,24 @@ using Test
             data = load_transactions(joinpath(@__DIR__,"files/data.txt"),',')
             @test size(data.matrix) == (9,16)
             @test sum(data.matrix) == 36
-            @test hash(sort(collect(values(data.colkeys)))) == UInt64(14154404351539088851)
-            @test hash(sort(collect(values(data.linekeys)))) == UInt64(1066772112083085456)
+            @test sort(collect(values(data.colkeys))) == ["bacon", "beer", "bread", "buns", "butter", "cheese", "eggs", "flour", "ham", "hamburger", "hot dogs", "ketchup", "milk", "mustard", "sugar", "turkey"]
+            @test sort(collect(values(data.linekeys))) == ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         end
 
         @testset "line indexes" begin
             data = load_transactions(joinpath(@__DIR__,"files/data_indexed.txt"),',';id_col = true)
             @test size(data.matrix) == (9,16)
             @test sum(data.matrix) == 36
-            @test hash(sort(collect(values(data.colkeys)))) == UInt64(14154404351539088851)
-            @test hash(sort(collect(values(data.linekeys)))) == UInt64(10356874651475808405)
+            @test sort(collect(values(data.colkeys))) == ["bacon", "beer", "bread", "buns", "butter", "cheese", "eggs", "flour", "ham", "hamburger", "hot dogs", "ketchup", "milk", "mustard", "sugar", "turkey"]
+            @test sort(collect(values(data.linekeys))) == ["1111", "1112", "1113", "1114", "1115", "1116", "1117", "1118", "1119"]
         end
 
         @testset "skip lines" begin
             data = load_transactions(joinpath(@__DIR__,"files/data_header.txt"),',';skiplines=2)
             @test size(data.matrix) == (9,16)
             @test sum(data.matrix) == 36
-            @test hash(sort(collect(values(data.colkeys)))) == UInt64(14154404351539088851)
-            @test hash(sort(collect(values(data.linekeys)))) == UInt64(1066772112083085456)
+            @test sort(collect(values(data.colkeys))) == ["bacon", "beer", "bread", "buns", "butter", "cheese", "eggs", "flour", "ham", "hamburger", "hot dogs", "ketchup", "milk", "mustard", "sugar", "turkey"]
+            @test sort(collect(values(data.linekeys))) == ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         end
     end
 
@@ -40,16 +40,16 @@ using Test
             data = transactions(dftest)
             @test size(data.matrix) == (9,16)
             @test sum(data.matrix) == 36
-            @test hash(sort(collect(values(data.colkeys)))) == UInt64(14154404351539088851)
-            @test hash(sort(collect(values(data.linekeys)))) == UInt64(1066772112083085456)
+            @test sort(collect(values(data.colkeys))) == ["bacon", "beer", "bread", "buns", "butter", "cheese", "eggs", "flour", "ham", "hamburger", "hot dogs", "ketchup", "milk", "mustard", "sugar", "turkey"]
+            @test sort(collect(values(data.linekeys))) == ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         end
 
         @testset "with index" begin
             data = transactions(dftest_index;indexcol=:Index)
             @test size(data.matrix) == (9,16)
             @test sum(data.matrix) == 36
-            @test hash(sort(collect(values(data.colkeys)))) == UInt64(14154404351539088851)
-            @test hash(sort(collect(values(data.linekeys)))) == UInt64(10356874651475808405)
+            @test sort(collect(values(data.colkeys))) == ["bacon", "beer", "bread", "buns", "butter", "cheese", "eggs", "flour", "ham", "hamburger", "hot dogs", "ketchup", "milk", "mustard", "sugar", "turkey"]
+            @test sort(collect(values(data.linekeys))) == ["1111", "1112", "1113", "1114", "1115", "1116", "1117", "1118", "1119"]
         end
 
     end
@@ -60,30 +60,28 @@ end
 
     @testset "percentage support" begin
         rules = apriori(data,0.3,5)
-        transform!(rules,:LHS =>( x -> hash.(x) ) => :SetHash)
-        sort!(rules,[:Length,:SetHash,:RHS])
-        @test hash(rules.LHS) == UInt64(10944680318112923216)
-        @test hash(rules.RHS) == UInt64(7170342362675143343)
-        @test hash(rules.Support) == UInt64(18329550519797518446)
-        @test hash(rules.Confidence) == UInt64(10378668557036289636)
-        @test hash(rules.Coverage) == UInt64(17208738295533172824)
-        @test hash(rules.Lift) == UInt64(18282341198944935968)
-        @test hash(rules.N) == UInt64(2389578521758638798)
-        @test hash(rules.Length) == UInt64(12370468415001452020)
+        sorted = sort(rules,[:Support,:RHS])
+        @test sorted.LHS == [String[], String[], String[], String[], ["milk"], ["eggs"], String[], String[]]
+        @test sorted.RHS == ["beer", "bread", "cheese", "ham", "eggs", "milk", "eggs", "milk"]
+        @test sorted.Support == [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.4444444444444444, 0.4444444444444444, 0.5555555555555556, 0.5555555555555556]
+        @test sorted.Confidence ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.7999999999999999, 0.7999999999999999, 0.5555555555555556, 0.5555555555555556]
+        @test sorted.Coverage ≈ [1.0, 1.0, 1.0, 1.0, 0.5555555555555556, 0.5555555555555556, 1.0, 1.0]
+        @test sorted.Lift ≈ [1.0, 1.0, 1.0, 1.0, 1.4399999999999997, 1.4399999999999997, 1.0, 1.0]
+        @test sorted.N == [3, 3, 3, 3, 4, 4, 5, 5]
+        @test sorted.Length == [1, 1, 1, 1, 2, 2, 1, 1]
     end
 
     @testset "absolute support" begin
         rules = apriori(data,2,5)
-        transform!(rules,:LHS =>( x -> hash.(x) ) => :SetHash)
-        sort!(rules,[:Length,:SetHash,:RHS])
-        @test hash(rules.LHS) == UInt64(10944680318112923216)
-        @test hash(rules.RHS) == UInt64(7170342362675143343)
-        @test hash(rules.Support) == UInt64(18329550519797518446)
-        @test hash(rules.Confidence) == UInt64(10378668557036289636)
-        @test hash(rules.Coverage) == UInt64(17208738295533172824)
-        @test hash(rules.Lift) == UInt64(18282341198944935968)
-        @test hash(rules.N) == UInt64(2389578521758638798)
-        @test hash(rules.Length) == UInt64(12370468415001452020)
+        sorted = sort(rules,[:Support,:RHS])
+        @test sorted.LHS == [String[], String[], String[], String[], ["milk"], ["eggs"], String[], String[]]
+        @test sorted.RHS == ["beer", "bread", "cheese", "ham", "eggs", "milk", "eggs", "milk"]
+        @test sorted.Support == [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.4444444444444444, 0.4444444444444444, 0.5555555555555556, 0.5555555555555556]
+        @test sorted.Confidence ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.7999999999999999, 0.7999999999999999, 0.5555555555555556, 0.5555555555555556]
+        @test sorted.Coverage ≈ [1.0, 1.0, 1.0, 1.0, 0.5555555555555556, 0.5555555555555556, 1.0, 1.0]
+        @test sorted.Lift ≈ [1.0, 1.0, 1.0, 1.0, 1.4399999999999997, 1.4399999999999997, 1.0, 1.0]
+        @test sorted.N == [3, 3, 3, 3, 4, 4, 5, 5]
+        @test sorted.Length == [1, 1, 1, 1, 2, 2, 1, 1]
     end
 end
 
@@ -92,21 +90,19 @@ end
     
     @testset "percentage support" begin
         sets = eclat(data,0.3)
-        transform!(sets,:Itemset =>( x -> hash.(x) ) => :SetHash)
-        sort!(sets,[:Length,:SetHash])
-        @test hash(sets.Itemset) == UInt64(9215540465155188081)
-        @test hash(sets.Support) == UInt64(9491076249976136591)
-        @test hash(sets.N) == UInt64(13538681268459648389)
-        @test hash(sets.Length) == UInt64(3041221529991437560)
+        sorted = sort(sets,[:N,:Itemset])
+        @test sets.Itemset == [["bread"], ["beer"], ["ham"], ["cheese"], ["milk"], ["milk", "eggs"], ["eggs"]]
+        @test sets.Support ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.5555555555555556, 0.4444444444444444, 0.5555555555555556]
+        @test sets.N == [3, 3, 3, 3, 5, 4, 5]
+        @test sets.Length == [1, 1, 1, 1, 1, 2, 1]
     end
     
     @testset "asbolute support" begin
         sets = eclat(data,3)
-        transform!(sets,:Itemset =>( x -> hash.(x) ) => :SetHash)
-        sort!(sets,[:Length,:SetHash])
-        @test hash(sets.Itemset) == UInt64(9215540465155188081)
-        @test hash(sets.Support) == UInt64(9491076249976136591)
-        @test hash(sets.N) == UInt64(13538681268459648389)
-        @test hash(sets.Length) == UInt64(3041221529991437560)
+        sorted = sort(sets,[:N,:Itemset])
+        @test sets.Itemset == [["bread"], ["beer"], ["ham"], ["cheese"], ["milk"], ["milk", "eggs"], ["eggs"]]
+        @test sets.Support ≈ [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.5555555555555556, 0.4444444444444444, 0.5555555555555556]
+        @test sets.N == [3, 3, 3, 3, 5, 4, 5]
+        @test sets.Length == [1, 1, 1, 1, 1, 2, 1]
     end
 end
