4d4e225be00f5e0accfc9524084d97fcc8138fce
JaredSchwartz <75581425+JaredSchwartz@users.noreply.github.com>
2025-05-26 12:13:32 -0600

    Consolidate itemset testing into a shared function

--- test/itemsets.jl
+++ test/itemsets.jl
@@ -8,203 +8,55 @@ function setsorter!(itemsets::DataFrame)
     select!(itemsets,Not(:SetHash))
 end
 
+# Helper function for testing algorithms with both percentage and absolute support
+function test_algorithms(algorithms, perc_sup, abs_sup, expected_items, expected_supports, expected_N, expected_length, data)
+    support_types = [("percentage support", perc_sup), ("absolute support", abs_sup)]
+    
+    for (alg_name, alg_func) in algorithms
+        @testset "$alg_name" begin
+            for (sup_name, sup_value) in support_types
+                @testset "$sup_name" begin
+                    sets = alg_func(data, sup_value)
+                    setsorter!(sets)
+                    @test sets.Itemset == expected_items
+                    @test sets.Support ≈ expected_supports
+                    @test sets.N == expected_N
+                    @test sets.Length == expected_length
+                end
+            end
+        end
+    end
+end
+
 @testset "frequent" begin
     # Define Frequent Itemset results at support of 3/0.3
-    freq_abs_sup = 3
-    freq_perc_sup = 0.3
     freq_items = [["beer"], ["bread"], ["cheese"], ["eggs"], ["ham"], ["milk"], ["eggs", "milk"]]
     freq_supports = [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.5555555555555556, 0.3333333333333333, 0.5555555555555556, 0.4444444444444444]
     freq_N = [3, 3, 3, 5, 3, 5, 4]
     freq_length = [1, 1, 1, 1, 1, 1, 2]
 
-    @testset "eclat.jl" begin
-        @testset "percentage support" begin
-            sets = eclat(data,freq_perc_sup)
-            setsorter!(sets)
-            @test sets.Itemset == freq_items
-            @test sets.Support ≈ freq_supports
-            @test sets.N == freq_N
-            @test sets.Length == freq_length
-        end
-        @testset "absolute support" begin
-            sets = eclat(data,freq_abs_sup)
-            setsorter!(sets)
-            @test sets.Itemset == freq_items
-            @test sets.Support ≈ freq_supports
-            @test sets.N == freq_N
-            @test sets.Length == freq_length
-        end
-    end
-    @testset "fpgrowth.jl" begin
-        @testset "percentage support" begin
-            sets = fpgrowth(data,freq_perc_sup)
-            setsorter!(sets)
-            @test sets.Itemset == freq_items
-            @test sets.Support ≈ freq_supports
-            @test sets.N == freq_N
-            @test sets.Length == freq_length
-        end
-        @testset "absolute support" begin
-            sets = fpgrowth(data,freq_abs_sup)
-            setsorter!(sets)
-            @test sets.Itemset == freq_items
-            @test sets.Support ≈ freq_supports
-            @test sets.N == freq_N
-            @test sets.Length == freq_length
-        end
-    end
-    @testset "recovery.jl" begin
-        @testset "recover_closed" begin
-            closed_sets = LCM(data,2)
-            sets = recover_closed(closed_sets,2)
-            remainder = subset(sets, :N => (x -> x .< freq_abs_sup))
-            subset!(sets, :N => (x -> x .>= freq_abs_sup))
-            setsorter!(sets)
-            setsorter!(remainder)
-            @test sets.Itemset == freq_items
-            @test sets.N == freq_N
-            @test sets.Length == freq_length
-            @test remainder.Itemset == [["bacon"], ["hamburger"], ["ketchup"], ["sugar"], ["bacon", "eggs"], ["beer", "hamburger"], ["beer", "milk"], ["bread", "ham"], ["cheese", "ham"], ["eggs", "sugar"], ["milk", "sugar"], ["eggs", "milk", "sugar"]]
-            @test remainder.N == [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-            @test remainder.Length == [1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3]
-        end
-        @testset "recover_maximal" begin
-            maximal_sets = genmax(data,freq_perc_sup)
-            sets = recover_maximal(maximal_sets)
-            setsorter!(sets)
-            @test sets.Itemset == freq_items
-            @test sets.Length == freq_length
-        end
-    end
+    algorithms = [("eclat.jl", eclat), ("fpgrowth.jl", fpgrowth)]
+    test_algorithms(algorithms, 0.3, 3, freq_items, freq_supports, freq_N, freq_length, data)
 end
 
 @testset "closed" begin
     # Define Closed Itemset results at support of 2/0.2
-    closed_abs_sup = 2
-    closed_perc_sup = 0.2
     closed_items = [["beer"], ["bread"], ["cheese"], ["eggs"], ["ham"], ["ketchup"], ["milk"], ["bacon", "eggs"], ["beer", "hamburger"], ["beer", "milk"], ["bread", "ham"], ["cheese", "ham"], ["eggs", "milk"], ["eggs", "milk", "sugar"]]
     closed_supports = [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.5555555555555556, 0.3333333333333333, 0.2222222222222222, 0.5555555555555556, 0.2222222222222222, 0.2222222222222222, 0.2222222222222222, 0.2222222222222222, 0.2222222222222222, 0.4444444444444444, 0.2222222222222222]
     closed_N = [3, 3, 3, 5, 3, 2, 5, 2, 2, 2, 2, 2, 4, 2]
     closed_length = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3]
 
-    @testset "fpclose.jl" begin
-        @testset "percentage support" begin
-            sets = fpclose(data,closed_perc_sup)
-            setsorter!(sets)
-            @test sets.Itemset == closed_items
-            @test sets.Support ≈ closed_supports
-            @test sets.N == closed_N
-            @test sets.Length == closed_length
-        end
-        @testset "absolute support" begin
-            sets = fpclose(data,closed_abs_sup)
-            setsorter!(sets)
-            @test sets.Itemset == closed_items
-            @test sets.Support ≈ closed_supports
-            @test sets.N == closed_N
-            @test sets.Length == closed_length
-        end
-    end
-    @testset "charm.jl" begin
-        @testset "percentage support" begin
-            sets = charm(data,closed_perc_sup)
-            setsorter!(sets)
-            @test sets.Itemset == closed_items
-            @test sets.Support ≈ closed_supports
-            @test sets.N == closed_N
-            @test sets.Length == closed_length
-        end
-        @testset "absolute support" begin
-            sets = charm(data,closed_abs_sup)
-            setsorter!(sets)
-            @test sets.Itemset == closed_items
-            @test sets.Support ≈ closed_supports
-            @test sets.N == closed_N
-            @test sets.Length == closed_length
-        end
-    end
-    @testset "carpenter.jl" begin
-        @testset "percentage support" begin
-            sets = carpenter(data,closed_perc_sup)
-            setsorter!(sets)
-            @test sets.Itemset == closed_items
-            @test sets.Support ≈ closed_supports
-            @test sets.N == closed_N
-            @test sets.Length == closed_length
-        end
-        @testset "absolute support" begin
-            sets = carpenter(data,closed_abs_sup)
-            setsorter!(sets)
-            @test sets.Itemset == closed_items
-            @test sets.Support ≈ closed_supports
-            @test sets.N == closed_N
-            @test sets.Length == closed_length
-        end
-    end
-    @testset "lcm.jl" begin
-        @testset "percentage support" begin
-            sets = LCM(data,closed_perc_sup)
-            setsorter!(sets)
-            @test sets.Itemset == closed_items
-            @test sets.Support ≈ closed_supports
-            @test sets.N == closed_N
-            @test sets.Length == closed_length
-        end
-        @testset "absolute support" begin
-            sets = LCM(data,closed_abs_sup)
-            setsorter!(sets)
-            @test sets.Itemset == closed_items
-            @test sets.Support ≈ closed_supports
-            @test sets.N == closed_N
-            @test sets.Length == closed_length
-        end
-    end
+    algorithms = [("fpclose.jl", fpclose), ("charm.jl", charm), ("carpenter.jl", carpenter), ("lcm.jl", LCM)]
+    test_algorithms(algorithms, 0.2, 2, closed_items, closed_supports, closed_N, closed_length, data)
 end
 
 @testset "maximal" begin
     # Define Maximal Itemset results at support of 3/0.3
-    max_abs_sup = 3
-    max_perc_sup = 0.3
     max_items = [["beer"], ["bread"], ["cheese"], ["ham"], ["eggs", "milk"]]
     max_supports = [0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.4444444444444444]
     max_N = [3, 3, 3, 3, 4]
     max_length = [1, 1, 1, 1, 2]
 
-    @testset "fpmax.jl" begin
-        @testset "percentage support" begin
-            sets = fpmax(data,max_perc_sup)
-            setsorter!(sets)
-            @test sets.Itemset == max_items
-            @test sets.Support ≈ max_supports
-            @test sets.N == max_N
-            @test sets.Length == max_length
-        end
-        @testset "absolute support" begin
-            sets = fpmax(data,max_abs_sup)
-            setsorter!(sets)
-            @test sets.Itemset == max_items
-            @test sets.Support ≈ max_supports
-            @test sets.N == max_N
-            @test sets.Length == max_length
-        end
-    end
-    @testset "genmax.jl" begin
-        @testset "percentage support" begin
-            sets = genmax(data,max_perc_sup)
-            setsorter!(sets)
-            @test sets.Itemset == max_items
-            @test sets.Support ≈ max_supports
-            @test sets.N == max_N
-            @test sets.Length == max_length
-        end
-        @testset "absolute support" begin
-            sets = genmax(data,max_abs_sup)
-            setsorter!(sets)
-            @test sets.Itemset == max_items
-            @test sets.Support ≈ max_supports
-            @test sets.N == max_N
-            @test sets.Length == max_length
-        end
-    end
-
+    algorithms = [("fpmax.jl", fpmax), ("genmax.jl", genmax)]
+    test_algorithms(algorithms, 0.3, 3, max_items, max_supports, max_N, max_length, data)
 end
